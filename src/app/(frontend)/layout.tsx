8880037240a857c44876e6e61cc5ec9ea2e5d9db
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2025-08-21 17:04:42 +0530

    refactor(layout): enhance layout structure and font integration
    - wrap RootLayout in ClerkProvider for authentication context
    - apply Geist font configuration to RootLayout and remove from FrontendLayout
    - streamline FrontendLayout by removing unnecessary components and classes
    - delete unused StudioLayout file to clean up the codebase

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,18 +1,5 @@
 import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/header";
-import { cn } from "@/lib/utils";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,13 +12,9 @@ export default function FrontendLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <section>
       <Header />
-      <div
-        className={cn(`${geistSans.variable} ${geistMono.variable} mx-auto max-w-5xl w-full antialiased`)}
-      >
-        {children}
-      </div>
-    </ClerkProvider>
+      <div className="mx-auto max-w-5xl w-full">{children}</div>
+    </section>
   );
 }
